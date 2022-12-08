b3a824647b61f3733d8d86adf9b8a3b77b9d1053
arfin-arif <arifmtbd@gmail.com>
2022-12-08 13:32:19 +0600

    basic server setup with mongoDB

---
+++ index.js
@@ -0,0 +1,33 @@
+const cors = require('cors');
+const express = require('express');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 8000;
+
+//middle ware 
+app.use(cors())
+app.use(express.json());
+
+// mDB
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.qktvmdh.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.log(error))
+
+
+
+app.get('/', (req, res) => {
+    res.send('Vouch Digital Server Is Running')
+});
+app.listen(port, () => {
+    console.log(`Vouch Digital Server running on port ${port}`)
+})
